ce6bc13c211159ce50e7559512dd81f7e50ff0de
perdreauarthur <arthur.perdreau@outlook.fr>
2025-06-19 16:47:16 +0200

    correctionEspace

--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -45,7 +45,11 @@ class EmailVerifier
      */
     public function handleEmailConfirmation(Request $request, User $user): void
     {
-        $this->verifyEmailHelper->validateEmailConfirmationFromRequest($request, (string) $user->getId(), (string) $user->getEmail());
+        $this->verifyEmailHelper->validateEmailConfirmationFromRequest(
+            $request,
+            (string) $user->getId(),
+            (string) $user->getEmail()
+        );
 
         $user->setIsVerified(true);
 
