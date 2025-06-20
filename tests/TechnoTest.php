31977185afef52dba4811bd72e307e37c4fb2d60
perdreauarthur <arthur.perdreau@outlook.fr>
2025-06-20 09:39:53 +0200

    testControllerHome

--- tests/TechnoTest.php
+++ tests/TechnoTest.php
@@ -2,12 +2,33 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
 
-class TechnoTest extends TestCase
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TechnoTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testDisplayTechnoForm()
     {
-        $this->assertTrue(true);
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/techno/new');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form');
+    }
+
+    public function testSubmitValidTechnoForm()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/techno/new');
+
+        $form = $crawler->selectButton('Submit')->form([
+            'techno_form[name]' => 'Symfony',
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseRedirects('/');
+        $client->followRedirect();
+        $this->assertSelectorExists('body');
     }
 }
---
+++ tests/homeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class homeTest extends WebTestCase
+{
+    public function testHomePageLoadsCorrectly(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'My projects');
+        $this->assertSelectorTextContains('h2', 'Contact me');
+        $this->assertSelectorExists('form[name="contact_form"]');
+    }
+
+    public function testSubmitContactFormSuccessfully(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $form = $crawler->filter('form[name="contact_form"]')->form([
+            'contact_form[name]' => 'Jean Test',
+            'contact_form[email]' => 'test@example.com',
+            'contact_form[message]' => 'Ceci est un test fonctionnel.'
+        ]);
+
+        $client->submit($form);
+        $this->assertResponseRedirects('/', 302);
+
+        $client->followRedirect();
+        $this->assertSelectorExists('.bg-green-500'); // ton flash message
+        $this->assertSelectorTextContains('.bg-green-500', 'Your message has been sent!');
+    }
+}
+
